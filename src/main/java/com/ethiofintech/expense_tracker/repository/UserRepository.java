44e7912313903e04a232a68b42313429a1deb3f2
Tsekag <kagtsega@gmail.com>
2026-01-08 20:21:25 +0300

    Refactor UserRepository package and add existsByEmail method

--- src/main/java/com/ethiofintech/expense_tracker/repository/UserRepository.java
+++ src/main/java/com/ethiofintech/expense_tracker/repository/UserRepository.java
@@ -1,10 +1,10 @@
-package com.ethiofintech.expense_tracker.repository;
+package com.ethio.et_tracker_api.repository;
 
-import com.ethiofintech.expense_tracker.model.User;
+import com.ethio.et_tracker_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Finds a user by their email (needed for Login)
     Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
