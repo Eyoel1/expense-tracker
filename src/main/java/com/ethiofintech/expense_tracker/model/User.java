6f5553777ec136f7752bba2e2fd17f814fcda55b
Tsekag <kagtsega@gmail.com>
2026-01-08 20:13:20 +0300

    updated user java

--- src/main/java/com/ethiofintech/expense_tracker/model/User.java
+++ src/main/java/com/ethiofintech/expense_tracker/model/User.java
@@ -1,31 +1,30 @@
-package com.ethiofintech.expense_tracker.model;
+package com.ethio.et_tracker_api.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.util.List;
+import lombok.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "users")
-@Data // Generates Getters, Setters, toString automatically
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String fullName;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
-    private String passwordHash;
+    private String password;
 
-    private String biometricId;
-
-    // Relationship: One User -> Many Transactions
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Transaction> transactions;
+    @Column(name = "created_at")
+    private Date createdAt = new Date();
 }
